d2a667073f68ae9e51b25d0f53092e860a20e91c
Daniel Perez <daniel.k.perez@gmail.com>
2020-12-11 03:09:51 -0600

    Initial commit.

---
+++ app/Console/Commands/ImportImages.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Seller;
+use League\Csv\Reader;
+use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Http;
+
+class ImportImages extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'images:import
+        {csvPath : The path to the CSV file}
+        {--headers= : Optional headers to use from the CSV file}
+        {--dryRun : Should this be a dry run}
+    ';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import and resize images for a seller CSV file.';
+
+    protected $bar;
+
+    protected $seller;
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $sellerName = $this->choice('Which seller is this for', Seller::pluck('name')->toArray());
+        $this->seller = Seller::where('name', $sellerName)->first();
+
+        $filePath = $this->argument('csvPath');
+
+        try {
+            $csv = Reader::createFromPath($filePath, 'r');
+        } catch (\Exception $e) {
+            $this->error('Unable to open file.');
+
+            return 0;
+        }
+
+        $csv->setHeaderOffset(0);
+        // perform header check
+        $headers = $this->option('headers') ?? ['Image 1', 'Image 2', 'Image 3', 'Image 4'];
+        // $header = $csv->getHeader();
+        // $this->info($header);
+        $records = $csv->getRecords(); //returns all the CSV records as an Iterator object
+
+        foreach ($records as $index => $record) {
+            $this->info(sprintf('Retrieving row: %s images...', $index));
+            $record = collect($record);
+            $urls = $record->filter(function ($item, $index) use ($headers) {
+                return in_array($index, $headers);
+            });
+            $this->fetchImagesFromUrls($urls);
+        }
+
+
+        return 0;
+    }
+
+    public function fetchImagesFromUrls(Collection $urls)
+    {
+        $good = [];
+        $bad = [];
+
+        $this->bar = $this->output->createProgressBar($urls->count());
+
+        $urls->each(function ($url) use ($good, $bad) {
+            try {
+                $good = $this->seller
+                    ->addMediaFromUrl($url)
+                    ->withResponsiveImages()
+                    ->toMediaCollection('bulk-upload-images');
+
+            } catch (\Exception $e) {
+                // $this->error(sprintf('Unable to fetch image %s', $url));
+                $bad[] = $url;
+            }
+
+            $this->bar->advance();
+        });
+
+        $this->bar->finish();
+
+        $this->info('');
+    }
+}
